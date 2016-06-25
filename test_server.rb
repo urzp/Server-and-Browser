63a26ed8075912d0d1a098d0e0a5d7864f4a7b9c
urzp <urzp002@mail.ru>
2016-06-25 20:26:04 +0300

    test for find out how work JSON

--- test_server.rb
+++ test_server.rb
@@ -1,12 +1,18 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.new('localhost', 3000)
 
 loop do
   socket = server.accept
-  while line = socket.gets 
-    puts line.chop 
-  end
+  request = socket.read_nonblock(256)
+  request_header, request_body = request.split("\r\n\r\n", 2) 
+  params = JSON.parse(request_body)
   
-socket.close  
+   puts params
+  puts params.class
+  
+  
+  
+  socket.close  
 end
