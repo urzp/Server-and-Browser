07a23e169acc95333476a1f2b628a2be88c88692
urzp <urzp002@mail.ru>
2016-06-25 20:05:11 +0300

    Create input GET/POST chose

--- Web_Browser.rb
+++ Web_Browser.rb
@@ -1,8 +1,22 @@
 require 'socket'
+require 'json'
  
+params = Hash.new { |hash, key| hash[key] = Hash.new }  
 host = 'localhost'     # The web server
 port = 3000                           # Default HTTP port
-path = "./index.html"                 # The file we want 
+path = "./index.html"               
+path = "./thanks.html"                 
+
+
+# input validation: require GET or POST
+#input = ''
+#until input == 'GET' || input == 'POST'
+#  print 'What type of request do you want to submit [GET, POST]? '
+#  input = gets.chomp
+#  input.upcase!
+#end
+
+input = 'POST'
 
 # This is the HTTP request we send to fetch a file
 request = "GET #{path} HTTP/1.0\r\n\r\n"
